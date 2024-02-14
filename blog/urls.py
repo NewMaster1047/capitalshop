8b97c7f368b7f6a5a16dd18a4388aba49aec2a4b
newmaster1047 <jaloliddinhamidov05@gmail.com>
2024-02-14 19:33:48 +0500

    all html files are connected to base

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
     path('pro_detals/', pro_details_view),
     path('checkout/', checkout_view),
     path('blog/', blog_view),
-    path('blog_details/', blog_details_view),
-    path('blog_details_/', blog_details_view_),
+    path('blog_details/', blog_details_view),  # blog-details.html
+    path('blog_details_/', blog_details_view_),  # blog_details.html
     path('elements/', elements_view),
     path('contact/', contact_view),
 
