2e14ff668bdbebe082f84517ae6ea4ef6a1c162f
newmaster1047 <jaloliddinmine00@gmail.com>
2024-02-19 21:01:02 +0500

    urls added

--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+
+
+
+
+
+
+    price = models.FloatField()
+    discount = models.IntegerField(default=0)
+    price_with_discount = models.FloatField(default=0)
+    brand = models.CharField(max_length=100)
+
+
+
+    def calculate_discount(self):
+        return (self.price / 100) * self.discount
+
+
+
+
+
+
+
+
+
--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def validate_uz_phone_number(phone_number: str):
+    uzb = phone_number.count('+998')
+
+    if uzb == 1:
+        print("True")
+    elif len(phone_number) != 13:
+        print("False")
+    else:
+        print("False")
+
+
+validate_uz_phone_number('+998901234567')
+
+
