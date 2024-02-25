4443ae0fcdb34e1e4cdc9b5ae18f54d69b9f4d87
jalol <jaloliddinmine00@gmail.com>
2024-02-25 19:59:27 +0500

    .

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='blog/')
+    tag = models.ManyToManyField(Tag)
+    description = models.TextField()
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
