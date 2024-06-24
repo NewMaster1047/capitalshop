aadba86e722b06b36d70c26ba990fed78264f6d8
NewMaster1047 <83957976+NewMaster1047@users.noreply.github.com>
2024-06-24 20:38:05 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +15,8 @@ urlpatterns = [
 
     path('login/', include('authentication.urls')),
 
-
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
 
 
